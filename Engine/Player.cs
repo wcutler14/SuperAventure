8a764307b4b9df09ca41e4ba72d03d07e569f452
wcutler <wcutler14@gmail.com>
2019-05-06 23:29:17 -0400

    Added a player class

---
+++ Engine/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Player
+    {
+        public int currentHitPoints { get; set; }
+        public int maxHitPoints { get; set; }
+        public int gold { get; set; }
+        public int experience { get; set; }
+        public int Level { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
